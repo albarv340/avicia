f64cd45aa0dfc744fac806f24bd7858929dee353
albarv340 <54350887+albarv340@users.noreply.github.com>
2021-01-29 00:22:32 +0100

    og tag test

--- time/time.js
+++ time/time.js
@@ -15,9 +15,10 @@ if (typeof (queryDict.ms) == "undefined" || !/^\d\d\%3A\d\d/.test(queryDict.ms))
     const hourString = queryDict.ms.substr(0, 2)
     const minute = Number(queryDict.ms.substr(5, 7))
     const minuteString = queryDict.ms.substr(5, 7)
-    const UTCTime = `${hourString}:${minuteString} UTC is`
+    const UTCTime = `${hourString}:${minuteString} UTC is `
     const timeString = new Date(new Date().setHours(hour, minute - new Date().getTimezoneOffset(), 0)).toTimeString()
     const time = timeString.substr(0, 8)
     const timeZone = timeString.substr(9, timeString.length)
+    document.querySelector('meta[name="title"]').setAttribute("content", UTCTime + time + " in " + timeZone);
     contentHTML.innerHTML = `<p>${UTCTime}</p><h1>${time}</h1><p>in ${timeZone}</p>`
 }
