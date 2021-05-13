4455720a5da1a2e2f351180fd3d20f4350ce1e80
albarv340 <54350887+albarv340@users.noreply.github.com>
2021-05-13 19:26:39 +0200

    NFR red

--- map/map.js
+++ map/map.js
@@ -125,7 +125,8 @@ async function run() {
     "the Avos": "#1010FF",
     "Upsilon": "#321e3b",
     "infilak": "#2dd6a6",
-    "Doki Doki Literature Club": "#c2b039"
+    "Doki Doki Literature Club": "#c2b039",
+    "Nefarious Ravens": "#990000"
   }
   //grabbing options elements
   let slider = document.getElementById("rate-option");
